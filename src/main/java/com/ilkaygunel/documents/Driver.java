791958818fbfa014d6e81a54afd1bff7d6709f12
ilkgunel93 <ilkgunel93@gmail.com>
2023-06-14 08:01:47 +0300

    Using @Data  annotation from Lombok instead of getters-setters.

--- src/main/java/com/ilkaygunel/documents/Driver.java
+++ src/main/java/com/ilkaygunel/documents/Driver.java
@@ -1,8 +1,10 @@
 package com.ilkaygunel.documents;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Data
 @Document("Driver")
 public class Driver {
 
@@ -18,30 +20,6 @@ public class Driver {
 
     public Driver(String name, String surname) {
         this.name = name;
-        this.surname = surname;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
         this.surname = surname;
     }
 
